c72fcf1f0c028aab03765ae23994bd12bd144723
Mihail <mihailmelinte74@gmail.com>
2024-07-19 19:17:07 +0300

    Add functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const digits = document.querySelectorAll(".digit");
+const equals = document.querySelector("#equals");
+const clearBtn = document.querySelector(".clearBtn");
 const operators = document.querySelectorAll(".operator");
-const equals = document.querySelector(".equals");
-let displayValue = document.querySelector("input");
-
+let displayValue = document.querySelector(".display");
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let choseSecondNumber = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -20,44 +24,90 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-let firstNumber, operator, secondNumber;
-
-function operate(firstNumber, operator, secondNumber) {
-    switch(operator) {
-        case "+":
-            return add(firstNumber,secondNumber);
-    
-        
+function operate(num1, op, num2) {
+    switch(op) {
+        case '+':
+            return add(num1,num2);
+            
         case "-":
-            return subtract(firstNumber,secondNumber);
-
+            return subtract(num1,num2);
 
         case "*":
-            return multiply(firstNumber,secondNumber);
-
+            return multiply(num1,num2);
 
         case "/":
-            return divide(firstNumber,secondNumber);
+            return divide(num1,num2);
     }
 }
 
-
-for(let i = 0; i < digits.length; i++) {
-    digits[i].addEventListener("click", () => {
-        displayValue.value += i+1;
+function chooseFirst(){
+    digits.forEach((digit) => {
+        digit.addEventListener("click", () => {
+            if(!choseSecondNumber){
+                if(firstNumber === null || displayValue.textContent === "0") {
+                    displayValue.textContent = digit.textContent;
+                } else {
+                    displayValue.textContent += digit.textContent;
+                }
+                firstNumber = Number(displayValue.textContent);
+            }
+        });
     });
 }
 
-for(let j = 0; j < operators.length; j++) {
-    operators[j].addEventListener("click", () => {
-        firstNumber = Number(displayValue.value);
-        operator = operators[j].textContent;
-        displayValue.value = "";
+function chooseOperator(){
+    operators.forEach((op) => {
+        op.addEventListener("click", () => {
+            if(firstNumber !== null) {
+                operator = op.textContent;
+                choseSecondNumber = true;
+                displayValue.textContent = "";
+            }
+        });
     });
 }
 
-equals.addEventListener("click", () => {
-    secondNumber = Number(displayValue.value);
-    let result = operate(firstNumber, operator, secondNumber);
-    displayValue.value = result;
-});
+function chooseSecond() {
+    digits.forEach((digit) => {
+        digit.addEventListener("click", () => {
+            if(choseSecondNumber){
+                if(displayValue === "0" || secondNumber === null) {
+                    displayValue.textContent = digit.textContent;
+                } else {
+                    displayValue.textContent += digit.textContent;
+                }
+                secondNumber = Number(displayValue.textContent); 
+            }
+        });
+    });
+}
+
+function calculate() {
+    equals.addEventListener("click", () => {
+        if(firstNumber !== null && operator !== null && secondNumber !== null) {
+            displayValue.textContent = operate(firstNumber, operator, secondNumber);
+            firstNumber = displayValue.textContent;
+        }
+    });
+}
+
+function resetDisplay() {
+    clearBtn.addEventListener("click", () => {
+        displayValue.textContent = "";
+        firstNumber = null;
+        secondNumber = null;
+        operator = null;
+        choseSecondNumber = false;
+    })
+}
+
+function updateDisplay() {
+    chooseFirst();
+    chooseOperator();
+    chooseSecond();
+    calculate();
+    resetDisplay();
+}
+
+updateDisplay();
+
