f871e633ffb07787d3cf8a785b14751e7caa6908
Mihail <mihailmelinte74@gmail.com>
2024-07-28 17:10:46 +0300

    Fix a bug

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let displayValue = "0";
 let choseSecond = false, choseOp = false;
 let firstNumber = null, operator = null, secondNumber = null;
 let lastOperator = null, lastSecondNumber = null;
-let roundDecimals = [];
+let roundDecimals = [], alreadyPressed = false;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -40,6 +40,15 @@ function operate(num1, op, num2){
 
         case "/":
             return divide(num1, num2);
+    }
+}
+
+function shortenDecimal() {
+    if(roundDecimals[1].length > 3) {
+        displayValue = String(Number(displayValue).toFixed(5));
+        display.textContent = displayValue;
+    } else {
+        display.textContent = displayValue;
     }
 }
 
@@ -75,44 +84,48 @@ function getOperator() {
 }
 
 function getSecondNumber() {
-    if(choseSecond) {
-        deleteLast();
-    }
     let check = false;
     for(let i = 0; i < numbers.length; i++) {
         numbers[i].addEventListener("click", () => {
-            if(choseSecond) {
-                if(displayValue === String(firstNumber) && !check) {
-                    displayValue = numbers[i].textContent;
-                    display.textContent = displayValue;
-                    check = true;
-                } else {
-                    displayValue += numbers[i].textContent;
-                    display.textContent = displayValue;
+            if(alreadyPressed) {
+                displayValue = "";
+                display.textContent = numbers[i].textContent;   
+                displayValue = display.textContent;
+                alreadyPressed = false;
+            }
+            else {
+                
+                if(choseSecond) {
+                    if(displayValue === String(firstNumber) && !check) {
+                        displayValue = numbers[i].textContent;
+                        display.textContent = displayValue;
+                        check = true;
+                    } else {
+                        displayValue += numbers[i].textContent;
+                        display.textContent = displayValue;
+                    }
+                    secondNumber = Number(displayValue); 
+                    choseOp = true;
                 }
-                secondNumber = Number(displayValue); 
-                choseOp = true;
+
             }
         });
     }
 }
+
+// IMPORTANT! FIX getResult FUNCTION!! IT BREAKS EVERYTHING!!!  
 
 function getResult() {
     equals.addEventListener("click", () => {
         if(choseOp) {
             displayValue = operate(firstNumber, operator, secondNumber);
             if(operator === "/" && String(secondNumber) === "0") {
+                displayValue = "";
                 display.textContent = "You can't divide by 0!";
-                equals.disabled = true;
             } else {
                 roundDecimals = String(displayValue).split(".");
                 if(roundDecimals[1] !== undefined) {
-                    if(roundDecimals[1].length > 3) {
-                        displayValue = String(Number(displayValue).toFixed(5));
-                        display.textContent = displayValue;
-                    } else {
-                        display.textContent = displayValue;
-                    }
+                    shortenDecimal();
                 } else {
                     display.textContent = displayValue;
                 }
@@ -127,17 +140,13 @@ function getResult() {
             displayValue = operate(firstNumber, lastOperator, lastSecondNumber);
             roundDecimals = String(displayValue).split(".");
             if(roundDecimals[1] !== undefined) {
-                if(roundDecimals[1].length > 3) {
-                    displayValue = String(Number(displayValue).toFixed(5));
-                    display.textContent = displayValue;
-                } else {
-                    display.textContent = displayValue;
-                }
+                shortenDecimal();
             } else {
                 display.textContent = displayValue;
             }
             display.textContent = displayValue;
             firstNumber = Number(displayValue);
+            alreadyPressed = true;
         }
     });
     for(let i = 0; i < operators.length; i++) {
@@ -146,12 +155,7 @@ function getResult() {
                 displayValue = operate(firstNumber, operator, secondNumber);
                 roundDecimals = String(displayValue).split(".");
                 if(roundDecimals[1] !== undefined) {
-                    if(roundDecimals[1].length > 3) {
-                        displayValue = String(Number(displayValue).toFixed(5));
-                        display.textContent = displayValue;
-                    } else {
-                        display.textContent = displayValue;
-                    }
+                    shortenDecimal();
                 } else {
                     display.textContent = displayValue;
                 }
@@ -194,20 +198,7 @@ function deleteLast() {
     });
 }
 
-function populateDisplay() {
-    getFirstNumber();
-    getOperator();
-    getSecondNumber();
-    getResult();
-    addDecimal();
-    deleteLast();
-}
-populateDisplay();
-
-
-
-
-/* function clearDisplay() {
+function clearDisplay() {
     clearBtn.addEventListener("click", () => {
         displayValue = "0";
         display.textContent = displayValue;
@@ -218,4 +209,13 @@ populateDisplay();
     });
 }
 
-clearDisplay(); */
+function populateDisplay() {
+    getFirstNumber();
+    getOperator();
+    getSecondNumber();
+    getResult();
+    addDecimal();
+    deleteLast();
+    clearDisplay();
+}
+populateDisplay();
